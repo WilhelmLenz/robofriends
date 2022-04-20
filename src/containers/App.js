92d8d0371501d7a72a8bb4b4fb6830d959175b3b
WilhelmLenz <wilhelm.lenz@neteleven.de>
2022-04-20 09:42:42 +0200

    update app

--- src/containers/App.js
+++ src/containers/App.js
@@ -37,11 +37,11 @@ class App extends Component {
             <h1 className="f1">Robofriends</h1>
             <SearchBox searchChange={this.onSearchChange} />
           </div>
-          <Scroll>
+          {/* <Scroll> */}
           <ErrorBoundry>
             <Cardlist robots={filterRobots}/>
           </ErrorBoundry>
-          </Scroll>
+          {/* </Scroll> */}
         </Fragment>
       )
   }
